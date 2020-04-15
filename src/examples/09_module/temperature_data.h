8a4c15fcf0416df40454e83aad283ea8e749696d
Thomas Greenway <TheTman234@gmail.com>
2020-04-15 18:21:55 -0500

    intro to pointers.

--- src/examples/09_module/temperature_data.h
+++ src/examples/09_module/temperature_data.h
@@ -1,1 +1,20 @@
 //temperature_data.h
+#include"temperature.h"
+#ifndef TEMPERATURE_DATA_H
+#define TEMPERATURE_DATA_H
+#include<iostream>
+#include<vector>
+#include<fstream>
+class TemperatureData
+{
+public:
+	void save_temps(std::vector<Temperature>& ts);
+	std::vector<Temperature> get_temps()const;
+
+private:
+
+	const std::string file_name{ "temperature.dat" };
+
+
+};
+#endif // !TEMPERATURE_DATA_H
