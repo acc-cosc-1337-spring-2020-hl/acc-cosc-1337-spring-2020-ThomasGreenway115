754a135eb070a17a835592c9e70766dd484543b0
Thomas Greenway <TheTman234@gmail.com>
2020-04-20 18:52:26 -0500

    Class Work 4/20

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,1 +1,19 @@
 //
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
+class Vector
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v); 
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i) const { return nums[i]; }
+private:
+	size_t size;
+	int* nums;
+};
+
+
+#endif // !MY_VECTOR_H
